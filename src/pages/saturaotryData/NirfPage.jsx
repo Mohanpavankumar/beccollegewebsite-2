fcafaed04c426421198c14abe929d3260938497e
vakacharitha <vaka.charitha@gmail.com>
2023-03-19 11:34:38 +0530

    nirf page changes

--- src/pages/saturaotryData/NirfPage.jsx
+++ src/pages/saturaotryData/NirfPage.jsx
@@ -1,3 +1,4 @@
+import { border } from '@chakra-ui/react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import { nirfData } from '../../Data/SaturatoryData/nirfData';
@@ -5,17 +6,24 @@ import './page.css';
 
 const Nirf = () => {
     return (
-        <Tabs
-            defaultActiveKey={nirfData.deafultKey}
-            id="justify-tab-example"
-            className="nirfPage-tabs mt-4 mb-1"
-        >
-            {nirfData.content.map(x => <Tab key={x.key} eventKey={x.key} title={x.tabTitle} className="nirf-tab-content">
-                <a href={x.link} target='_blank' rel='noopener noreferrer'>
-                    {x.contentTitle}
-                </a>
-            </Tab>)}
-        </Tabs>
+        <div className='d-flex justify-between  mt-4 mb-1'>
+            <div>
+                <Tabs
+                    defaultActiveKey={nirfData.deafultKey}
+                    id="justify-tab-example"
+                    className="nirfPage-tabs ml-2"
+                >
+                    {nirfData.content.map(x => <Tab key={x.key} eventKey={x.key} title={x.tabTitle} className="nirf-tab-content">
+                        <a href={x.link} target='_blank' rel='noopener noreferrer'>
+                            {x.contentTitle}
+                        </a>
+                    </Tab>)}
+                </Tabs>
+            </div>
+            <div style={{ border: "1px solid red", padding: "1rem", margin:"1rem" }}>
+                1
+            </div>
+        </div >
     );
 }
 
