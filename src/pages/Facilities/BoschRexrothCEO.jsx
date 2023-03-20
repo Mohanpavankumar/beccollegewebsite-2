167172a18b82e35494cb5a6e38a36ec95257d62c
shaheensk <110378392+shaheensk@users.noreply.github.com>
2023-03-20 23:18:10 +0530

    updates

--- src/pages/Facilities/BoschRexrothCEO.jsx
+++ src/pages/Facilities/BoschRexrothCEO.jsx
@@ -133,7 +133,7 @@ export default function Facilities(){
 																<div class="relative w-full h-full max-w-6xl md:h-full">
 																	<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 																		<div class="flex items-center justify-between p-3 border-b rounded-t dark:border-gray-600">
-																			<p lassName="text-2xl ml-3 font-semibold">
+																			<p className="text-2xl ml-3 font-semibold">
 																						Training Modules
 																			</p>
 																			<button type="button" class="absolute top-3 right-2.5 text-black  hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-200 dark:hover:text-black" data-modal-hide="popup-modal">
