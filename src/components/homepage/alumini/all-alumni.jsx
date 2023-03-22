a802e12d67df12b923cb360cea4ebda970b37885
Padmaja-Vinta <98469970+Padmaja-Vinta@users.noreply.github.com>
2023-03-22 23:57:05 +0530

    chg

--- src/components/homepage/alumini/all-alumni.jsx
+++ src/components/homepage/alumini/all-alumni.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-// import { aluminiData } from "../../../Data/homepage/aluminiData";
+import { aluminiData } from "../../../Data/homepage/aluminiData";
 
 let AllAlumni = (props) => {
 	return (
 		<div className="d-flex justify-around ">
-			<Card style={{ width: "14rem" }}>
-				<Card.Img
-					variant="top"
-					src={props.item.profileImage}
-					style={{ height: "14rem" }}
-				/>
-				<Card.Body className="text-center ">
-					<Card.Title>{x.name}</Card.Title>
-					<Card.Text>{x.description}</Card.Text>
-					<Card.Subtitle className="p-3">{x.companyName}</Card.Subtitle>
-				</Card.Body>
-			</Card>
+			{!!aluminiData &&
+				aluminiData.map((x) => (
+					<Card style={{ width: "14rem" }}>
+						<Card.Img
+							variant="top"
+							src={x.profileImage}
+							style={{ height: "14rem" }}
+						/>
+						<Card.Body className="text-center ">
+							<Card.Title>{x.name}</Card.Title>
+							<Card.Text>{x.description}</Card.Text>
+							<Card.Subtitle className="p-3">{x.companyName}</Card.Subtitle>
+						</Card.Body>
+					</Card>
+				))}
 		</div>
 	);
 };
